8c660841699f7a15a75113f98991524260b343aa
yangjian <yangjian@3bu.cn>
2021-08-26 15:26:39 +0800

    change command

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'https://github.com/yj666/react-vite-h5.git',
       path: '/home/react-vite-h5',
-      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
         NODE_ENV: 'production'
       }
