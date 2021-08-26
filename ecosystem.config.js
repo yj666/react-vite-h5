36b2f194c07b3973603e0c1cde15c3d28b04270b
yangjian <yangjian@3bu.cn>
2021-08-26 14:36:12 +0800

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'react-vite-h5',
+      script: 'react-vite-h5-server.js'
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '106.75.87.103',
+      ref: 'origin/master',
+      repo: 'https://github.com/yj666/react-vite-h5.git',
+      path: '/workspace/react-vite-h5',
+      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+}
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+// router/index.js
+
+import Home from '@/container/Home'
+import Data from '@/container/Data'
+import User from '@/container/User'
+import Login from '@/container/Login'
+
+const routes = [
+  {
+    path: "/",
+    component: Home
+  },
+  {
+    path: "/data",
+    component: Data
+  },
+  {
+    path: "/user",
+    component: User
+  },
+  {
+    path: "/login",
+    component: Login
+  },
+];
+
+export default routes
