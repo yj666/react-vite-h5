bedc038e6fd85106f0f84c5c0ef3503c40faf1f4
yangjian <yangjian@3bu.cn>
2021-08-28 12:15:57 +0800

    合并

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ import { Toast } from 'zarm'
 
 const MODE = import.meta.env.MODE // 环境变量
 
-axios.defaults.baseURL = MODE == 'development' ? '/api' : 'http://106.75.87.103:7001/api/'
+axios.defaults.baseURL = MODE == 'development' ? '/api' : 'http://106.75.87.103/api/'
 axios.defaults.withCredentials = true
 axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers['Authorization'] = `${localStorage.getItem('token') || null}`
